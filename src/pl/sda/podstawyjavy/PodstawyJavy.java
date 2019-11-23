6abbfa8455afb5f7695c4fc069bf2912bceee7b8
Lukasz <lukasio99@poczta.fm>
2019-11-23 16:00:05 +0100

    Inkrementacja

--- src/pl/sda/podstawyjavy/PodstawyJavy.java
+++ src/pl/sda/podstawyjavy/PodstawyJavy.java
@@ -2,13 +2,15 @@ package pl.sda.podstawyjavy;
 
 public class PodstawyJavy {
     public static void main(String[] args) {
-        int[][] mojaTablicaInt = {{1, 2, 3}, {3, 4, 5}, {5, 7, 8}};
+      int i = 0;
+        System.out.println(i); // sprawdzenie i
+        System.out.println(++i); // zwiekszamy o 1 przed wykonaniem kodu
+        System.out.println(i); // sprawdzenie i
+        System.out.println(i++); // zwiekszamy o 1 po wykonaniu kodu
+        System.out.println(i); // sprawdzenie i
 
-        for (int i = 0; i < mojaTablicaInt.length; i++){
-            for (int j = 0; j < mojaTablicaInt[i].length; j++) {
-                System.out.println(mojaTablicaInt[i][j]);
-            }
-        }
+        System.out.println(i); // drukuje wartosc 2 po wykonaniu wczesniejszych instrukcji
+
     }
     void wydrukujArgumenty(String[] args) {
 
