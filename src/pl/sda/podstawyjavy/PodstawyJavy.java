55cf59e11881aa0fa49aa2a616f3cff00872eace
Lukasz <lukasio99@poczta.fm>
2019-11-24 10:39:38 +0100

    Klasa Scanner - czytanie wartosci z klawiatury uzytkownika

--- src/pl/sda/podstawyjavy/PodstawyJavy.java
+++ src/pl/sda/podstawyjavy/PodstawyJavy.java
@@ -1,13 +1,22 @@
 package pl.sda.podstawyjavy;
 
+import java.util.Scanner;
+
 public class PodstawyJavy {
     public static void main(String[] args) {
 
-       int yob = 1980;
+        for (int i = 0; i <5; i++) {
 
-       String powitanie = (yob ==1980 ) ? "Urodziles sie w 1980" : "Nie urodziles sie w 1980" ;
-        System.out.println(powitanie);
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.println("Prosze podaj rok urodzenia:");
+
+
+            int yob = scanner.nextInt();
+
+            String powitanie = (yob == 1980) ? "Urodziles sie w 1980" : "Nie urodziles sie w 1980";
+            System.out.println(powitanie);
+        }
     }
     void wydrukujArgumenty(String[] args) {
 
